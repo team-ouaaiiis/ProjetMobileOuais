600407de4f9f214796d88581e97ca6ac5939b608
Nicolo <nicoceriani@gmail.com>
2019-02-12 20:36:26 +0100

    Chunk Manager started / Imported TMPro Essentials

---
+++ Assets/Scripts/Inheritance/Entities/Environments/NonInteractable/Chunk.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : NonInteractable
+{
+    [SerializeField] private List<Entity> entities = new List<Entity>();
+}
---
+++ Assets/Scripts/Inheritance/Entities/Managers/ChunkManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : Manager
+{
+    #region Fields 
+
+    [Header("Components")]
+    [SerializeField] private List<Chunk> chunks = new List<Chunk>();
+    [SerializeField] private Transform chunkScroller;
+
+    [Header("Parameters")]
+    [SerializeField] private float scrollSpeed = 5f;
+
+    #endregion
+
+    #region Monobehaviour Callbacks
+
+    public override void Update()
+    {
+        base.Update();
+        Scrolling();
+    }
+
+    #endregion
+
+    private void Scrolling()
+    {
+        float zScroll = Time.deltaTime * scrollSpeed;
+        chunkScroller.position += new Vector3(0, 0, zScroll);
+    }
+
+    #region Properties
+
+    public float ScrollSpeed { get => scrollSpeed; set => scrollSpeed = value; }
+
+    #endregion
+}
