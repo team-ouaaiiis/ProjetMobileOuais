916e9e01f1d661991762e79487e11729b4b21aeb
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-02-12 22:37:01 +0100

    Prototyping Materials
    Des materials avec des textures utiles pour prototyper
    Début de script pour l'arme

---
+++ Assets/Scripts/Inheritance/Entities/WeaponScripts/PlayerWeapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : Interactable
+{
+
+    public Weapon weapon;
+    
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+
+    public bool IsThrown
+    {
+        get
+        {
+            return weapon.isThrown;
+        }
+        set
+        {
+            if (value == true)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            weapon.isThrown = value;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Inheritance/Entities/WeaponScripts/WeaponClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Weapon
+{
+    public float damagePoint = 1;
+    public float range = 1;
+    public bool isEquipped;
+    public bool isThrown;
+}
