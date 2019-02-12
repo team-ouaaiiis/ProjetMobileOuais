bffdaa87e9cc306064d4b47fe4be6d6a981b12be
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-02-13 00:36:05 +0100

    Rangement et clean de la scene

--- Assets/Scripts/Inheritance/Entities/WeaponScripts/PlayerWeapon.cs
+++ Assets/Scripts/Inheritance/Entities/WeaponScripts/PlayerWeapon.cs
@@ -6,14 +6,20 @@ public class PlayerWeapon : Interactable
 {
 
     public Weapon weapon;
+
+    [Header("Movement")]
+    public CurveTrajectory trajectory;
+    [Range(0,2)]
+    [Tooltip("throw time before the weapon reach the other character")]
+    [SerializeField] float throwTime = 0.2f; // le throw time before the weapon reach the other character
+    float currentThrowTime = 0;
+
     
-    // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
     }
 
-    // Update is called once per frame
     public override void Update()
     {
         base.Update();
