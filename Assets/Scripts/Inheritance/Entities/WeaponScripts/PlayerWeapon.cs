b2b1a5469ba1b71b6b9122922e3af840773fa22b
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-02-14 00:29:54 +0100

    début script player attack system

--- Assets/Scripts/Inheritance/Entities/WeaponScripts/PlayerWeapon.cs
+++ Assets/Scripts/Inheritance/Entities/WeaponScripts/PlayerWeapon.cs
@@ -5,7 +5,9 @@ using NaughtyAttributes;
 
 public class PlayerWeapon : Interactable
 {
+    #region Fields
 
+    [Space(20)]
     public Weapon weapon;
 
     [Header("Weapon Movement")]
@@ -23,10 +25,37 @@ public class PlayerWeapon : Interactable
     public ThrowDirection throwDirection;
     [SerializeField] AnimationCurve speedEvolution;
 
-    
+    [Header("Weapon Attack range")]
+    [SerializeField] Vector3 attackBoxOffset;
+    Vector3 attackBoxSize = Vector3.one;
+
+    [Header("Weapon Attack values")]
+    [ReadOnly] public bool isAttacking;
+    [SerializeField] [Range(0.0f,1.0f)] float attackDelay = 0.0f;
+    [SerializeField] [Range(0.0f,1.0f)] float attackDuration = 0.2f;
+    [SerializeField] LayerMask attackTargetLayer;
+    IEnumerator attack;
+
+    //[Header("Debugs")]
+
+    [BoxGroup("Debug Colors")]
+    [SerializeField] bool showDebugBox = true;
+
+    [ShowIf("showDebugBox")]
+    [BoxGroup("Debug Colors")]
+    [SerializeField] Color attackBoxColor = Color.red;
+
+    [ShowIf("showDebugBox")]
+    [BoxGroup("Debug Colors")]
+    [SerializeField] Color throwSphereColor = Color.blue;
+
+
+    #endregion
+
     public override void Start()
     {
         base.Start();
+        SetAttackBoxSize();
     }
 
     public override void Update()
@@ -35,7 +64,35 @@ public class PlayerWeapon : Interactable
         ThrowTimer();
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        if (!showDebugBox) return;
+        SetAttackBoxSize();
+
+        //alpha settings
+        float boxAlpha = 0.4f;
+        if (isAttacking) boxAlpha = 0.6f;
+
+        //attackBoxSize box
+        Gizmos.color = new Color(attackBoxColor.r, attackBoxColor.g,attackBoxColor.b,boxAlpha);
+        Gizmos.DrawCube(transform.position + attackBoxOffset, attackBoxSize);
+        Gizmos.color = new Color(attackBoxColor.r, attackBoxColor.g, attackBoxColor.b, 1);
+        Gizmos.DrawWireCube(transform.position + attackBoxOffset, attackBoxSize);
+
+        //attack box throw
+        Gizmos.color = new Color(throwSphereColor.r, throwSphereColor.g, throwSphereColor.b, 0.4f);
+        Gizmos.DrawSphere(transform.position, weapon.throwRadiusRange);
+        Gizmos.color = new Color(throwSphereColor.r, throwSphereColor.g, throwSphereColor.b, 1);
+        Gizmos.DrawWireSphere(transform.position, weapon.throwRadiusRange);
+    }
+
+    void SetAttackBoxSize()
+    {
+        attackBoxSize = new Vector3(attackBoxSize.x, attackBoxSize.y, weapon.range);
+    }
+
     #region Throw functions
+
     public bool IsThrown
     {
         get
@@ -74,12 +131,19 @@ public class PlayerWeapon : Interactable
         }
     }
 
+    /// <summary>
+    /// Cancel the throw movement of the weapon 
+    /// </summary>
     public void CancelThrow()
     {
         IsThrown = false;
         currentThrowTime = 0;
     }
 
+    /// <summary>
+    /// start Throwing weapon in a choosen direction
+    /// </summary>
+    /// <param name="_throwDirection"></param>
     public void ThrowWeapon(ThrowDirection _throwDirection)
     {
         throwDirection = _throwDirection;
@@ -94,6 +158,9 @@ public class PlayerWeapon : Interactable
         }
     }
 
+    /// <summary>
+    /// Start throwing weapon
+    /// </summary>
     [Button("Throw Weapon")]
     public void ThrowWeapon()
     {
@@ -133,6 +200,62 @@ public class PlayerWeapon : Interactable
                 throwDirection = ThrowDirection.Right;
                 break;
         }
+    }
+
+    #endregion
+
+    #region Attack functions
+
+    [Button]
+    public void StartAttack()
+    {
+        if (isAttacking)
+        {
+            Debug.LogWarning("Already attacking");
+            return;
+        }
+
+        attack = Attacking();
+        StartCoroutine(attack);
+        isAttacking = true;
+    }
+
+    public void CancelAttack()
+    {
+        if(attack != null)
+        {
+            StopCoroutine(attack);
+        }
+
+        isAttacking = false;
+    }
+
+    void Attack()
+    {
+        Collider[] colliders = Physics.OverlapBox(transform.position + attackBoxOffset, attackBoxSize / 2, Quaternion.identity, attackTargetLayer, QueryTriggerInteraction.Collide);
+        Debug.Log("ATTACKING");
+
+        if (colliders.Length > 0)
+        {
+            Debug.Log("Somehting detected");
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                IDamageListener damageListener = colliders[i].GetComponent<IDamageListener>();
+                if(damageListener != null)
+                {
+                    damageListener.TakeDamage(weapon.damagePoint);
+                    Debug.Log("Damaging something");
+                }
+            }
+        }
+    }
+
+    IEnumerator Attacking()
+    {
+        yield return new WaitForSeconds(attackDelay);
+        Attack();
+        yield return new WaitForSeconds(attackDuration);
+        isAttacking = false;
     }
 
     #endregion
--- Assets/Scripts/Inheritance/Entities/WeaponScripts/WeaponClass.cs
+++ Assets/Scripts/Inheritance/Entities/WeaponScripts/WeaponClass.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Weapon
 {
     public float damagePoint = 1;
-    public float range = 1;
     public bool isEquipped;
     public bool isThrown;
+    [Header("Attack Range")]
+    public float range = 1;
+    public float throwRadiusRange = 1;
 }
