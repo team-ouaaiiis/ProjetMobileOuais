eb41bbe86c93cfec3c0997e0a314ae84cd60b00e
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-02-25 09:58:01 +0100

    FIX

--- Assets/Scripts/Monobehaviour/Inheritance/Entities/Environments/NonInteractable/Chunk.cs
+++ Assets/Scripts/Monobehaviour/Inheritance/Entities/Environments/NonInteractable/Chunk.cs
@@ -1,77 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Chunk : NonInteractable
-{
-    #region Fields
-
-    [Header("Chunk")]
-    [SerializeField] private List<Entity> chunkMembers = new List<Entity>();
-    private ChunkManager chunkManager;
-    private ChunkPattern chunkPattern;
-
-    #endregion
-
-    #region Monobehaviour Callbacks
-
-    public override void Start()
-    {
-        base.Start();
-        chunkManager = GameManager.instance.ChunkManager;
-    }
-
-    public override void Update()
-    {
-        base.Update();
-        CheckDistance();
-        Scrolling();
-    }
-    
-    #endregion
-
-    #region Private Methods
-
-    private void CheckDistance()
-    {
-        if (transform.position.z <= -chunkManager.ChunkSize)
-        {
-            DeactivateChunk();
-        }
-    }
-    
-    private void Scrolling()
-    {
-        float zScroll = Time.deltaTime * chunkManager.ScrollSpeed;
-        transform.localPosition -= new Vector3(0, 0, zScroll);
-    }
-
-    private void DeactivateChunk()
-    {
-        Debug.Log("Deactivate");
-        GameManager.instance.ChunkManager.SpawnNewChunk();
-        gameObject.SetActive(false);
-    }
-
-    #endregion
-
-    #region Public Methods
-
-    public void InitializeChunk(ChunkPattern pattern)
-    {
-        chunkPattern = pattern;
-
-        for (int i = 0; i < chunkPattern.ChunkElements.Count; i++)
-        {
-             
-        }
-    }
-
-    #endregion
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -166,4 +93,3 @@ public class Chunk : NonInteractable
 
     #endregion
 }
->>>>>>> develop
