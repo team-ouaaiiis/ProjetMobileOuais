7f3fd3f68fc324d620980e4aa85073ce61765dc2
Nicolo <nicoceriani@gmail.com>
2019-02-27 23:21:29 +0100

    map

---
+++ Assets/Scripts/Monobehaviour/TechArt/CustomDirectionalLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomDirectionalLight : MonoBehaviour
+{
+    [SerializeField] private List<Material> mats = new List<Material>();
+    [SerializeField] private Color lightCol = Color.white;
+    [SerializeField] private Color shadowColor = Color.gray;
+    [SerializeField] private float lightIntensity = 0f;
+    [SerializeField] private float shadowStrength = 0.2f;
+
+    private void Update()
+    {
+        for (int i = 0; i < mats.Count; i++)
+        {
+            mats[i].SetVector("_LightDir", transform.forward);
+            mats[i].SetColor("_AddColor", lightCol);
+            mats[i].SetColor("_ShadowColor", shadowColor);
+            mats[i].SetFloat("_AddedStrength", lightIntensity);
+            mats[i].SetFloat("_ShadowStrength", shadowStrength);
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.yellow);
+    }
+}
---
+++ Assets/Scripts/Monobehaviour/TechArt/ProceduralMapTextureReader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class ProceduralMapTextureReader : MonoBehaviour
+{
+    [SerializeField] private RenderTexture rt;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Color cityColor;
+    [SerializeField] private float range = 0.05f;
+    private Texture2D tex2D;
+
+    [Button]
+    private void Check()
+    {
+        tex2D = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+
+        Rect rectReadPicture = new Rect(0, 0, rt.width, rt.height);
+
+        RenderTexture.active = rt;
+
+        // Read pixels
+        tex2D.ReadPixels(rectReadPicture, 0, 0);
+        tex2D.Apply();
+
+        for (int x = 0; x < tex2D.width; x++)
+        {
+            for (int y = 0; y < tex2D.height; y++)
+            {
+                Debug.Log("Analysing");
+                float r = tex2D.GetPixel(x, y).r;
+                float g = tex2D.GetPixel(x, y).g;
+                float b = tex2D.GetPixel(x, y).b;
+                if(IsInRange(cityColor.r, range, r) && IsInRange(cityColor.g, range, g) && IsInRange(cityColor.b, range, b))
+                {
+                    Debug.Log("Detected City");
+                }
+            }
+        }
+
+        RenderTexture.active = null; // added to avoid errors 
+    }
+
+    private bool IsInRange(float baseFloat, float range, float checkedFloat)
+    {
+        if (checkedFloat <= baseFloat + range && checkedFloat > baseFloat - range)
+        {
+            return true;
+        }
+
+        else
+            return false;
+    }
+}
