2524542830f0f97f1131f7486af109c0f59383ac
Nicolo <nicoceriani@gmail.com>
2019-02-28 21:39:41 +0100

    Map Controls Wip

--- Assets/Scripts/Monobehaviour/TechArt/WorldMap/WorldMapCamera.cs
+++ Assets/Scripts/Monobehaviour/TechArt/WorldMap/WorldMapCamera.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class WorldMapCamera : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private GameObject target;
+    [SerializeField] private Camera cam;
+
+    [Header("Settings")]
+    [SerializeField] private float sensitivity = 5f;
+    [SerializeField] private float smoothCam = 0.2f;
+    [SerializeField] private float zoomAmnt = 10f;
+    private Vector3 currentVelCam;
     private bool isMoving = false;
-    private Vector2 initialPos;
+    private Vector2 initialPosPointer;
+    private Vector3 initialPosMap;
+    private float initialDistance;
 
     private void Update()
     {
@@ -14,36 +25,67 @@ public class WorldMapCamera : MonoBehaviour
     }
 
     private void Position()
-    {        
-        if(isMoving)
+    {
+        Vector3 diff = new Vector3();
+#if UNITY_EDITOR
+        diff = (new Vector3(ConvertedMousePos().x, 0, ConvertedMousePos().y) - new Vector3(initialPosPointer.x, 0, initialPosPointer.y));
+#endif
+
+#if UNITY_ANDROID
+        if(Input.touchCount > 0 )
+            diff = (new Vector3(TouchPos(0).x, 0, TouchPos(0).y) - new Vector3(initialPosPointer.x, 0, initialPosPointer.y));
+
+        if (Input.touchCount >= 2)
         {
-            transform.position += new Vector3(ConvertedMousePos().x, 0, ConvertedMousePos().z) - new Vector3(initialPos.x, 0, initialPos.y);
+            cam.orthographicSize = 50 + (1f / (initialDistance - Vector3.Distance(TouchPos(0), TouchPos(1))) * zoomAmnt);
         }
+#endif
+
+        if (isMoving)
+        {
+            target.transform.position = initialPosMap + diff * sensitivity;
+        }
+        cam.transform.position = Vector3.SmoothDamp(cam.transform.position, target.transform.position, ref currentVelCam, smoothCam);
     }
 
     private void MouseDetection()
     {
-        #if UNITY_ANDROID
+#if UNITY_ANDROID
 
-        //if (Input.GetTouch(0).phase == TouchPhase.Began)
-        //{
-        //    isMoving = true;
-        //    initialPos = ConvertedTouchPos();
-        //}
+        if(Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                isMoving = true;
+                initialPosPointer = TouchPos(0);
+                initialPosMap = target.transform.position;
+            }
 
-        //if (Input.GetTouch(0).phase == TouchPhase.Ended)
-        //{
-        //    isMoving = false;
-        //}
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                isMoving = false;
+            }
+            
+            if (Input.touchCount > 1)
+            {
+                if (Input.GetTouch(1).phase == TouchPhase.Began)
+                {
+                    initialDistance = Vector3.Distance(TouchPos(0), TouchPos(1));
+                }
+            }
+        }
 
-        #endif
+        
 
-        #if UNITY_EDITOR
+#endif
 
-        if(Input.GetMouseButtonDown(0))
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButtonDown(0))
         {
             isMoving = true;
-            initialPos = ConvertedMousePos();
+            initialPosPointer = ConvertedMousePos();
+            initialPosMap = target.transform.position;
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -51,16 +93,23 @@ public class WorldMapCamera : MonoBehaviour
             isMoving = false;
         }
 
-        #endif
+ #endif
     }
 
-    private Vector3 ConvertedTouchPos()
+    private Vector3 TouchPos(int i)
     {
-        return Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        if(Input.touchCount > 0)
+        {
+            return new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, transform.position.z);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
     }
 
     private Vector3 ConvertedMousePos()
     {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return Input.mousePosition;
     }
 }
