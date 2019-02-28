d2743cd8f74f0ccedf806166df94d39a63fcaeb3
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-02-28 12:59:56 +0100

    Repulse

--- Assets/Scripts/Monobehaviour/Inheritance/Entities/Characters/Enemy.cs
+++ Assets/Scripts/Monobehaviour/Inheritance/Entities/Characters/Enemy.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 
-public class Enemy : Character, IDamageListener
+public class Enemy : Character, IDamageListener, IRepulseListener
 {
+    [BoxGroup("References"), SerializeField] Collider enemyCollider;
+
     [Header("Enemy values")]
     public float attackRange = 1;
     [SerializeField] Vector3 hitboxSize = Vector3.one;
     [SerializeField] public Vector3 hitboxOffset;
     [SerializeField] LayerMask playerTargetLayer;
+    [SerializeField] LayerMask enemyLayer;
+
+    [Header("Receive damage movement")]
+    [SerializeField] float hitBackDistance = 1;
+    [SerializeField] AnimationCurve movementCurve;
+    [SerializeField] float movementTime = 0.5f;
+    [ReadOnly] public bool onDamageMovement;
+    float currentMoveTime;
+    Vector3 iniPos;
+    Vector3 targetPos;
+    List<Collider> touchedColliders = new List<Collider>();
 
     [BoxGroup("Debug"), SerializeField] Color hitboxColor = Color.cyan;
-    
+
+
+    public override void Start()
+    {
+        base.Start();
+        IniTouchedColliders();
+    }
+    public override void Update()
+    {
+        base.Update();
+        ReceiveDamageMovement();
+    }
 
     public virtual void Attack()
     {
@@ -33,11 +57,72 @@ public class Enemy : Character, IDamageListener
         }
     }
 
+    public void Repulsion()
+    {
+        Collider[] colliders = Physics.OverlapBox(transform.position + hitboxOffset, hitboxSize / 2, Quaternion.identity, enemyLayer, QueryTriggerInteraction.Collide);
+
+        if (colliders.Length > 0)
+        {
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (!touchedColliders.Contains(colliders[i]))
+                {
+                    touchedColliders.Add(colliders[i]);
+                    IRepulseListener repulseListener = colliders[i].GetComponent<IRepulseListener>();
+                    if (repulseListener != null)
+                    {
+                        repulseListener.Repulse();
+                        Debug.Log("Repulse something");
+                    }
+                }
+            }
+        }
+    }
+
+    public void Repulse()
+    {
+        StartHitMovement();
+    }
+
+    
     public override void TakeDamage(float dmg)
     {
         base.TakeDamage(dmg);
+        Repulse();
     }
 
+    [Button]
+    public void StartHitMovement()
+    {
+        targetPos = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + hitBackDistance);
+        iniPos = transform.localPosition;
+        onDamageMovement = true;
+    }
+
+    public virtual void ReceiveDamageMovement()
+    {
+        if (!onDamageMovement) return;
+
+        currentMoveTime += Time.deltaTime;
+        float percent = currentMoveTime / movementTime;
+        float t = movementCurve.Evaluate(percent);
+
+        transform.localPosition = Vector3.Lerp(iniPos, targetPos, t);
+        Repulsion();
+
+        if(currentMoveTime >= movementTime)
+        {
+            currentMoveTime = 0.0f;
+            onDamageMovement = false;
+            IniTouchedColliders();
+        }
+    }
+
+    private void IniTouchedColliders()
+    {
+        touchedColliders = new List<Collider>(); //reset list
+        touchedColliders.Add(enemyCollider);
+    }
 
     public void OnDrawGizmos()
     {
@@ -47,12 +132,11 @@ public class Enemy : Character, IDamageListener
         Gizmos.DrawCube(transform.position + hitboxOffset, hitboxSize);
     }
 
-
     public override void Death()
     {
         base.Death();
 
         //DEBUG TEST
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 }
