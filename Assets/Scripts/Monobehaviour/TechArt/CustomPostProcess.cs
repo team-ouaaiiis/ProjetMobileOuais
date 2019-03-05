aac2dc483070fdeb672c939a77366308afa3a65f
Nicolo <nicoceriani@gmail.com>
2019-03-05 21:35:45 +0100

    outline SHADFERer

---
+++ Assets/Scripts/Monobehaviour/TechArt/CustomPostProcess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomPostProcess : MonoBehaviour
+{
+    [SerializeField] private Material mat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+    
+}
