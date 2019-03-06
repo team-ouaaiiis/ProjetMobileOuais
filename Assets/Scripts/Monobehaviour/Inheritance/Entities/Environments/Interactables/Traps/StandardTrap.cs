adabc00802305b646d2a2b9a943da049f742d779
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-03-06 16:54:27 +0100

    Ouais le piège
    Premier piège basique codey et fonctionnel

---
+++ Assets/Scripts/Monobehaviour/Inheritance/Entities/Environments/Interactables/Traps/StandardTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class StandardTrap : Trap
+{
+
+    public Projectile[] projectiles;
+
+
+    public override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    public override void TakeDamage(float dmg)
+    {
+        base.TakeDamage(dmg);
+        AttackProjectile();
+    }
+
+    [Button]
+    void AttackProjectile()
+    {
+        for (int i = 0; i < projectiles.Length; i++)
+        {
+            projectiles[i].StartAttack();
+        }
+    }
+
+
+
+    
+
+
+}
