a98fe71df3f0de75463d96ec0c0252d7114627e8
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-03-07 16:31:20 +0100

    AudioManager

--- Assets/Scripts/Monobehaviour/Inheritance/Entities/Managers/AudioManager.cs
+++ Assets/Scripts/Monobehaviour/Inheritance/Entities/Managers/AudioManager.cs
@@ -14,13 +14,38 @@ public class AudioManager : Manager
         [Range(0.0f, 1.0f)] public float volume = 1.0f;
         [Range(-3.0f, 3.0f)] public float pitch = 1;
         [Range(0, 256)] public int priority = 128;
+
+
+        [HideInInspector] public GameObject soundObject;
+        [HideInInspector] public AudioSource audioSource;
+
+        public void PlaySound()
+        {
+            audioSource.panStereo = 0;
+            audioSource.Play();
+        }
+
+        public void PlaySoundAt(float _xPos)
+        {
+            _xPos = Mathf.Clamp(_xPos, -1, 1);
+            audioSource.panStereo = _xPos;
+            audioSource.Play();
+        }
+
+        public void StopSound()
+        {
+            audioSource.Stop();
+        }
+
     }
 
     public Sound[] sounds;
 
-    [ReadOnly] public List<AudioSource> audioSources = new List<AudioSource>();
+    [ReadOnly] public Dictionary<string,Sound> soundDictionary = new Dictionary<string, Sound>();
 
     public static AudioManager instance;
+
+    #region Monobehaviour Callbacks
 
     public override void Awake()
     {
@@ -28,10 +53,12 @@ public class AudioManager : Manager
         instance = this;
     }
 
+    #endregion
+
     [Button]
-    public void CreateSounds()
+    void CreateSounds()
     {
-        if (audioSources.Count > 0)
+        if (soundDictionary.Count > 0)
         {
             DeleteAllSounds();
         }
@@ -41,6 +68,7 @@ public class AudioManager : Manager
             //create game object
             GameObject audioGO = new GameObject(sounds[i].name);
             audioGO.transform.SetParent(transform);
+            sounds[i].soundObject = audioGO;
 
             //Create & configure audio source
             AudioSource source = audioGO.AddComponent<AudioSource>();
@@ -49,26 +77,39 @@ public class AudioManager : Manager
             source.volume = sounds[i].volume;
             source.pitch = sounds[i].pitch;
             source.priority = sounds[i].priority;
+            sounds[i].audioSource = source;
 
             //Add audio source to list
-            audioSources.Add(source);
-
+            soundDictionary.Add(sounds[i].name,sounds[i]);
         }
     }
 
     [Button]
-    public void DeleteAllSounds()
+    void DeleteAllSounds()
     {
-        for (int i = 0; i < audioSources.Count; i++)
+        for (int i = 0; i < sounds.Length; i++)
         {
-            DestroyImmediate(audioSources[i].gameObject);
+            DestroyImmediate(sounds[i].soundObject);
         }
 
-        audioSources = new List<AudioSource>();
+        soundDictionary = new Dictionary<string, Sound>();
     }
 
-    public void PlaySound()
+    #region Public Methods
+
+    /// <summary>
+    /// Play sound
+    /// </summary>
+    /// <param name="_soundName">The sname of the sound to play</param>
+    public void PlaySound(string _soundName)
     {
-
+        soundDictionary[_soundName].PlaySound();
     }
+
+    public void StopSound(string _soundName)
+    {
+        soundDictionary[_soundName].StopSound();
+    }
+
+    #endregion
 }
