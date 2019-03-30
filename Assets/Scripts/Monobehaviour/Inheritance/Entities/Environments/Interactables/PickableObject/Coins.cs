de4c23d1323f2f93218243203e89e3a1983dbaa6
VINCENT TRINEL <v.trinel@rubika-edu.com>
2019-03-30 19:48:38 +0100

    Coins

---
+++ Assets/Scripts/Monobehaviour/Inheritance/Entities/Environments/Interactables/PickableObject/Coins.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : PickableObject
+{
+    [Header("Coins values")]
+    [SerializeField] float value;
+
+    public override void GetObject()
+    {
+        base.GetObject();
+
+    }
+}
